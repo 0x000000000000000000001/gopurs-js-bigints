190269719cd01dac63e6ef456e125c4801167efb
0x1 <kevin.francart3@gmail.com>
2026-08-09 03:44:22 +0200

    Update Purescript BigInt FFI to Go

--- output/Data.Number.Format/Data_Number_Format_ffi.go
+++
@@ -1,8 +0,0 @@
-package Data_Number_Format
-
-import "gopurs/output/gopurs_runtime"
-
-var _Gopurs_ToExponentialNative = gopurs_runtime.Func(func(_ gopurs_runtime.Value) gopurs_runtime.Value { panic("FFI not implemented: toExponentialNative"); return gopurs_runtime.Value{} })
-var _Gopurs_ToFixedNative = gopurs_runtime.Func(func(_ gopurs_runtime.Value) gopurs_runtime.Value { panic("FFI not implemented: toFixedNative"); return gopurs_runtime.Value{} })
-var _Gopurs_ToPrecisionNative = gopurs_runtime.Func(func(_ gopurs_runtime.Value) gopurs_runtime.Value { panic("FFI not implemented: toPrecisionNative"); return gopurs_runtime.Value{} })
-var _Gopurs_ToString = gopurs_runtime.Func(func(_ gopurs_runtime.Value) gopurs_runtime.Value { panic("FFI not implemented: toString"); return gopurs_runtime.Value{} })
